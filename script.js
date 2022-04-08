14aad53c80b448f5c5e1c48cf5ee44b51d5ac70a
Nathan Graham <nathangraham301@yahoo.com>
2022-04-08 10:41:57 -0400

    added alert when contact form button is clicked

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-const name = document.getElementById('nameF')
-const email = document.getElementById('email')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (nameF.value === '' || nameF.value == null) {
-        messages.push('Name is required')
+  const name = document.getElementById('nameF')
+  const email = document.getElementById('email')
+  const form = document.getElementById('form')
+  const errorElement = document.getElementById('error')
+  
+  form.addEventListener('submit', (e) => {
+     e.preventDefault();
+  
+      let messages = []
+      if (nameF.value === '' || nameF.value == null) {
+          messages.push('Name is required')
+      }
+  
+      if (email.value.length <= 0) {
+          messages.push('Email is required')
+      }
+  
+      if (messages.length > 0) {
+          errorElement.innerText = messages.join(', ')
+      }
+    else{
+      alert("conatct information submitted successfully!");
+      return;
     }
-
-    if (email.value.length <= 0) {
-        messages.push('Email is required')
-    }
-
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
-})
+  })
+    
+  
+    
+    
